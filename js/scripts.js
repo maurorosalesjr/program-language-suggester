c49e212bb9ed9a905f9c0138704ea341e289307b
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-04-07 11:02:06 -0700

    add date form to html, and js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$("form#pls").submit(function(event) {
+  event.preventDefault();
+  const exp = $("input#exp").val();
+  const today = $("#today").val()
+  
+  
+  
+  
+  if("#experienced") {
+    $("#experienced").alert("Maybe you're too advanced for this quiz and should probably learn something harder then Java Script, C# or Python")
+  }
+  
+});
