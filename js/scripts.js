fc76335433907ac3d422a0a80771796cdd2add9d
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-04-07 11:38:37 -0700

    add color picker and radio forms to html and their scripts to js. also add the  begining of results paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 $("form#pls").submit(function(event) {
   event.preventDefault();
   const exp = $("input#exp").val();
-  const today = $("#today").val()
+  const today = $("#today").val();
+  const favoriteColor = $("#color").val();
+  const space = $("#input:radio[name=space]:checked").val()
   
   
   
   
   if("#experienced") {
     $("#experienced").alert("Maybe you're too advanced for this quiz and should probably learn something harder then Java Script, C# or Python")
+  }
+
+  function colorValue(color) {
+    if(color === "red") {
+      value = 2
+    } else if (color === "blue") {
+      value = 3
+    } else if (color === "green") {
+      value = 4
+    } else {
+      value = 1
+    }
   }
   
 });
