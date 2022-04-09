c1dc070d73ea5d471174e10f8f9bb85cef30093d
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-04-09 12:28:34 -0700

    further debugging

--- js/scripts.js
+++ js/scripts.js
@@ -5,42 +5,34 @@ $("form#pls").submit(function(event) {
   const color = $("#color").val();
   const space = $("#input:radio[name=space]:checked").val();
   const fightInput = $("input#fightInput").val();
-  const google = $("input#google").val();
-  /*const result = add(exp, favoriteColor, space, google);
-    $("#output").text(result);*/
+  const google = $("input#google").val();*/
   const number1 = parseInt($("#exp").val());
-  /*const number4 = parseInt($("#color").val());*/
   const number2 = parseInt($("#space").val());
   const number3 = parseInt($("#google").val());
   const result = add(number1, number2, number3,);
-  $("#output").text(result);
-  
-
-  /*function color(color) {
-    if(color === "red") {
-      value = 2
-    } else if (color === "blue") {
-      value = 3
-    } else if (color === "green") {
-      value = 4
-    } else {
-      value = 1
-    }
-  }*/
   
   function add(number1, number2, number3) {
-    return number1, number2, number3
+    return number1 + number2 + number3
   }
 
+  if (result < 3) {
+    ("#javaScript").show();
+    ("#c#").hide();
+    ("#python").hide();
+  } else if (result > 6) {
+    ("#javaScript").hide();
+    ("#c#").hide();
+    ("#python").show();
+  } else {
+    ("#javaScript").hide();
+    ("#c#").show();
+    ("#python").hide();
+  }
 
+  $("#output").text(result);
   $("#result").show();
-console.log(add)
-  /*$(document).ready(function() {
-    $("form#add").submit(function(event) {
-      event.preventDefault();
-        
-    });
-  });*/
+
+
 
 
   /*if("") {
