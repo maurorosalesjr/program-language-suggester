dc9135bba61924b83afc92be3164186982ce26f0
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-04-09 14:08:09 -0700

    more debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,33 @@
 $("form#pls").submit(function(event) {
   event.preventDefault();
-  /*const exp = $("input#exp").val();
+  const exp = parseInt($("input#exp").val());
   const today = $("#today").val();
   const color = $("#color").val();
-  const space = $("#input:radio[name=space]:checked").val();
+  const space = parseInt($("#input:radio[name=space]:checked").val());
   const fightInput = $("input#fightInput").val();
-  const google = $("input#google").val();*/
-  const number1 = parseInt($("#exp").val());
-  const number2 = parseInt($("#space").val());
-  const number3 = parseInt($("#google").val());
-  const result = add(number1, number2, number3,);
+  const google = parseInt($("input#google").val());
+  const result = add(exp, space, google);
   
-  function add(number1, number2, number3) {
-    return number1 + number2 + number3
+  function add(exp, space, google) {
+    return exp + space + google;
   }
+  console.log(exp);
+  
 
-  if (result < 3) {
-    ("#javaScript").show();
-    ("#c#").hide();
+  if (result <= 3) {
+    ("#jS").show();
+    ("#c-Sharp").hide();
     ("#python").hide();
   } else if (result > 6) {
-    ("#javaScript").hide();
-    ("#c#").hide();
+    ("#jS").hide();
+    ("#c-Sharp").hide();
     ("#python").show();
   } else {
-    ("#javaScript").hide();
-    ("#c#").show();
+    ("#jS").hide();
+    ("#c-Sharp").show();
     ("#python").hide();
   }
 
   $("#result").text(result);
-  $("#result").show();
-
-
-
-
-  /*if("") {
-    $("#experienced").alert("Maybe you're too advanced for this quiz and should probably learn something harder then Java Script, C# or Python")
-  }*/
-  
+  $("result").show();
 });
