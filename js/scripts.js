4e6d350d366af454519206dd75423160cb1e37be
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-04-09 15:37:52 -0700

    fixed google space inputs and code cleaning

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,32 @@
-$("form#pls").submit(function(event) {
-  event.preventDefault();
-  const exp = parseInt($("input#exp").val());
-  const today = $("#today").val();
-  const color = $("#color").val();
-  const space = parseInt($("#input:radio[name=space]:checked").val());
-  const fightInput = $("input#fightInput").val();
-  const google = parseInt($("input#google").val());
-  const result = add(exp, space, google);
-  
-  function add(exp, space, google) {
-    return exp + space + google;
-  }
-  console.log(exp);
-  
 
-  if (result <= 3) {
-    ("#jS").show();
-    ("#c-Sharp").hide();
-    ("#python").hide();
-  } else if (result > 6) {
-    ("#jS").hide();
-    ("#c-Sharp").hide();
-    ("#python").show();
-  } else {
-    ("#jS").hide();
-    ("#c-Sharp").show();
-    ("#python").hide();
-  }
+$(document).ready(function() {
+  $("form#pls").submit(function(event) {
+    event.preventDefault();
+    const exp = parseInt($("#exp").val());
+    const today = $("#today").val();
+    const color = $("#color").val();
+    const space = parseInt($("input:radio[name=space]:checked").val());
+    const fightInput = $("input#fightInput").val();
+    const google = parseInt($("input:radio[name=google]:checked").val());
+    const result = add(exp, space, google);
+    function add(exp, space, google) {
+      return exp + space + google;
+    }
 
-  $("#result").text(result);
-  $("result").show();
+    $("#jS").hide();
+    $("#c-Sharp").hide();
+    $("#python").hide();
+    
+
+    if (result <= 3) {
+      $("#jS").show();
+    } else if (result > 6) {
+      $("#python").show();
+    } else {
+      $("#c-Sharp").show();
+    }
+
+    $("#result").show();
+  });
 });
+
